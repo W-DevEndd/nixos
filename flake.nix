5ea346905d7a4254c2c4d87ecfd0fcf5dd96c62f
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-06 14:31:40 +0700

    fix: syntaxx error

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
         home-manager = {
             url = "github:nix-community/home-manager/release-26.05";
             inputs.nixpkgs.follows = "nixpkgs";
-        }
+        };
     };
 
     outputs = { self, nixpkgs, home-manager, ... }: {
