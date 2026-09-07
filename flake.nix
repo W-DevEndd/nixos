016e5b73e5fc67beb5fac106261d807515f16bef
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-07 14:06:53 +0700

    ??

--- flake.nix
+++ flake.nix
@@ -14,14 +14,14 @@
             modules = [
                 ./configuration.nix
                 home-manager.nixosModules.home-manager
-                {
-                    home-manager = {
-                        useGlobalPkgs = true;
-                        useUserPackages = true;
-                        users."w-devendd" = import ./home/w-devendd.nix;
-                        backupFileExtension = "bak";
-                    };
-                }
+                # {
+                #     home-manager = {
+                #         useGlobalPkgs = true;
+                #         useUserPackages = true;
+                #         users."w-devendd" = import ./home/w-devendd.nix;
+                #         backupFileExtension = "bak";
+                #     };
+                # }
             ];
         };
     };
