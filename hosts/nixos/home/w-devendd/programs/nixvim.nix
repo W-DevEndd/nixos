14abbe17cbf952bc4f2fc817df560d5578950fc9
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-11 14:22:35 +0700

    feat: init nixvim v3

--- hosts/nixos/home/w-devendd/programs/nixvim.nix
+++ hosts/nixos/home/w-devendd/programs/nixvim.nix
@@ -3,7 +3,11 @@
     imports = [ inputs.nixvim.homeModules.nixvim ];
 
     programs.nixvim = {
+        version.enableNixpkgsReleaseCheck = false;
+        nixpkgs.source = inputs.nixpkgs;
+
         enable = true;
+        defaultEditor = true;
 
         colorschemes.catppuccin.enable = true;
         plugins.lualine.enable = true;
