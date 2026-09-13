753fb6f80b01a17b9a07305b6bf347bc0d6a9bde
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-13 11:22:02 +0700

    feat: config some nixvim and add amd ucode

--- hosts/nixos/home/w-devendd/programs/nixvim.nix
+++ hosts/nixos/home/w-devendd/programs/nixvim.nix
@@ -10,7 +10,10 @@
         enable = true;
         defaultEditor = true;
 
+        # -----
         # Core
+        # -----
+
         opts = {
             number = true;
             relativenumber = true;
@@ -28,71 +31,112 @@
             cursorline = true;
         };
 
+        # -----
         # Theme
+        # -----
+
         colorschemes.catppuccin = {
             enable = true;
 
             settings.flavour = "mocha";
         };
-
+       
+        # -----
         # Plugins
+        # -----
+
+        # Depend
         plugins.web-devicons.enable = true;
 
+        # UI
         plugins.lualine.enable = true;
+        plugins.telescope.enable = true;
         plugins.barbar.enable = true;
-        plugins.neo-tree = {
-            enable = true;
-            settings.filesystem = {
-                useLibuvFileWatcher = true;
-                follow_current_file = {
-                    enabled = true;
-                    leave_dirs_open = true;
-                };
+
+        plugins.neo-tree.enable = true;
+        plugins.neo-tree.settings.filesystem = {
+            useLibuvFileWatcher = true;
+            follow_current_file = {
+                enabled = true;
+                leave_dirs_open = true;
             };
         };
 
-        plugins.telescope.enable = true;
+        # Coding
+        plugins.treesitter.enable = true;
+        plugins.treesitter.nixGrammars = false; 
+        plugins.treesitter.settings = {
+            auto_install = true; 
+            ensure_installed = [ ]; 
+            highlight.enable = true;
+        };
+
         plugins.blink-cmp.enable = true;
-        plugins.blink-cmp.settings = {
-            appearance = {
-                nerd_font_variant = "normal";
-                use_nvim_cmp_as_default = true;
-            };
-            completion = {
-                accept = {
-                    auto_brackets = {
-                        enabled = true;
-                        semantic_token_resolution = {
-                            enabled = false;
+        plugins.blink-cmp.settings.keymap = {
+            "<C-b>" = [
+                "scroll_documentation_up"
+                    "fallback"
+            ];
+            "<C-e>" = [
+                "hide"
+            ];
+            "<C-f>" = [
+                "scroll_documentation_down"
+                    "fallback"
+            ];
+            "<C-n>" = [
+                "select_next"
+                    "fallback"
+            ];
+            "<C-p>" = [
+                "select_prev"
+                    "fallback"
+            ];
+            "<C-space>" = [
+                "show"
+                    "show_documentation"
+                    "hide_documentation"
+            ];
+            "<C-y>" = [
+                "select_and_accept"
+            ];
+            "<Down>" = [
+                "select_next"
+                    "fallback"
+            ];
+            "<S-Tab>" = [
+                "snippet_backward"
+                    "fallback"
+            ];
+            "<Tab>" = [
+                "snippet_forward"
+                    "fallback"
+            ];
+            "<Up>" = [
+                "select_prev"
+                    "fallback"
+            ];
+        };
+        plugins.blink-cmp.settings.completion.menu.border = "rounded";
+        plugins.blink-cmp.settings.signature.window.border = "rounded";
+        plugins.blink-cmp.settings.completion.documentation.window.border = "rounded";
+
+        # -----
+        # LSP
+        # -----
+
+        lsp.servers.nixd.enable = true;
+        lsp.servers.nixd.config = {
+            settings = {
+                nixd = {
+                    options = {
+                        nixos = {
+                            expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.hostname.options";
                         };
                     };
                 };
-                documentation = {
-                    auto_show = true;
-                };
-            };
-            keymap = {
-                preset = "super-tab";
-            };
-            signature = {
-                enabled = true;
-            };
-            sources = {
-                cmdline = [ ];
-                providers = {
-                    buffer = {
-                        score_offset = -7;
-                    };
-                    lsp = {
-                        fallbacks = [ ];
-                    };
-                };
             };
         };
-
-
-        # LSP
-        lsp.servers.nixd.enable = true;
         lsp.servers.pyright.enable = true;
         lsp.servers.ccls.enable = true;
     };
--- hosts/nixos/perfomance.nix
+++ hosts/nixos/perfomance.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, config, ... }:
 {
+
+    hardware.cpu.amd.updateMicrocode = true;
+
     hardware.graphics = {
         enable = true;
         enable32Bit = true;
