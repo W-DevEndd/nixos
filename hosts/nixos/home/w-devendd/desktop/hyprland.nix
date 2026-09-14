79481938161874aba003c62a58256eaef609f24c
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-14 14:07:32 +0700

    refatctor: clean non-nix language

--- hosts/nixos/home/w-devendd/desktop/hyprland.nix
+++ hosts/nixos/home/w-devendd/desktop/hyprland.nix
@@ -3,8 +3,6 @@
     wayland.windowManager.hyprland.enable = true;
     wayland.windowManager.hyprland.xwayland.enable = true;
 
-    wayland.windowManager.hyprland.extraConfig = builtins.readFile ./hyprland.lua;
-
     imports = [
         ../programs/kitty.nix
     ];
