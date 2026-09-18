d75eb0f43b93bdb570b4f1d334cfe32fe41efbb4
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-18 14:24:40 +0700

    refactor: flake input

--- flake.nix
+++ flake.nix
@@ -1,24 +1,27 @@
 {
     description = "Hello, NixOS!";
     inputs = {
+        # Nix Repos
         nixpkgs.url = "nixpkgs/nixos-26.05";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixvim = {
-            url = "github:nix-community/nixvim";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        quickshell = {
-            url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        home-manager = {
-            url = "github:nix-community/home-manager/release-26.05";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+
+        home-manager.url = "github:nix-community/home-manager/release-26.05";
+
+        # Standalone Repos
+        nixvim.url = "github:nix-community/nixvim";
+        quickshell.url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+            
+
+        # Somethings.....
         w-devendd-dotfiles = {
             url = "github:w-devendd/dotfiles";
             flake = false;
         };
+
+        # Follows
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+        nixvim.inputs.nixpkgs.follows = "nixpkgs";
+        quickshell.inputs.nixpkgs.follows = "nixpkgs";
     };
 
     outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixvim, ... } @inputs: {
