032c9b720e58dc490ba6ca90fbfd6857b3ba1e94
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-19 07:06:42 +0700

    feat: add grim, satty for hyprland depends

--- hosts/nixos/home/w-devendd/desktop/hyprland.nix
+++ hosts/nixos/home/w-devendd/desktop/hyprland.nix
@@ -2,7 +2,7 @@
 {
     xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink "${config."w-devendd".dotfilesRoot}/hypr";
 
-    home.packages = with pkgs; [kdePackages.dolphin];
+    home.packages = with pkgs; [kdePackages.dolphin grim satty];
 
     imports = [
         ../programs/kitty.nix
