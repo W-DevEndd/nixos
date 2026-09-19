721f49689050cd9ff852b10ddf58d84d7adc04ab
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-19 10:53:19 +0700

    feat: add some catppuccin theme

--- hosts/nixos/home/w-devendd/desktop/catppuccin.nix
+++ hosts/nixos/home/w-devendd/desktop/catppuccin.nix
@@ -6,10 +6,12 @@
     catppuccin.accent = "red";
     catppuccin.flavor = "mocha";
 
-    catppuccin.qt5ct.enable  = true;
-    catppuccin.kitty.enable  = true;
-    catppuccin.obs.enable    = true;
-    catppuccin.btop.enable   = true;
-    catppuccin.cava.enable   = true;
-    catppuccin.fcitx5.enable = true;
+    catppuccin.qt5ct.enable   = true;
+    catppuccin.kitty.enable   = true;
+    catppuccin.obs.enable     = true;
+    catppuccin.btop.enable    = true;
+    catppuccin.cava.enable    = true;
+    catppuccin.fcitx5.enable  = true;
+    catppuccin.vesktop.enable = true;
+    catppuccin.kvantum.enable = true;
 }
