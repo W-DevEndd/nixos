8848319a961370ec49c903def45a5c234a16350c
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-20 13:56:02 +0700

    feat: add hypr dynamic cursor plugin

--- hosts/laptop/services/hyprland.nix
+++ hosts/laptop/services/hyprland.nix
@@ -4,4 +4,5 @@
         enable = true;
         xwayland.enable = true;
     };
+    environment.etc."hypr/plugins/libhypr-dynamic-cursors.so".source = "${pkgs.hyprlandPlugins.hypr-dynamic-cursors}/lib/libhypr-dynamic-cursors.so";
 }
