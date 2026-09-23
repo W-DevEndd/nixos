8c744b64b27bdb1af9249cc27b22a95f605615ad
W-DevEndd <111259650+W-DevEndd@users.noreply.github.com>
2026-09-24 06:16:59 +0700

    feat: add lib qtimage-formats lib

--- hosts/ideapad/home/w-devendd/desktop/quickshell.nix
+++ hosts/ideapad/home/w-devendd/desktop/quickshell.nix
@@ -5,7 +5,7 @@
         package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
     };
     home.packages = with pkgs; [
-        qt6.qtmultimedia ffmpeg
+        qt6.qtmultimedia qt6.qtimageformats ffmpeg
     ];
     xdg.configFile."quickshell".source = config.lib.file.mkOutOfStoreSymlink "${config."w-devendd".dotfilesRoot}/quickshell";
 }
